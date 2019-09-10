5bfeb9f645dc616140f4d3172f5c16694ffadc6e
YjjTT <yjjtt@tempot.com>
2019-09-10 16:45:58 +0800

    html-webpack-plugin index.html自动刷新

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,1 +1,4 @@
 console.log('hi2')
+const div = document.createElement('div')
+div.innerText = 'div'
+document.body.appendChild(div)
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     mode: 'production',
     entry: {
@@ -17,5 +17,10 @@ module.exports = {
                 loader: 'awesome-typescript-loader'
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        })
+    ]
 }
