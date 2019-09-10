f26da1d0287f4b92480a8c6cb6a8c5cb25009902
YjjTT <yjjtt@tempot.com>
2019-09-10 17:10:53 +0800

    安装了react和react-dom

---
+++ lib/button.tsx
@@ -0,0 +1,4 @@
+function Button() {
+  return <div>按钮</div>;
+}
+export default Button;
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,4 +1,5 @@
-console.log('hi2')
-const div = document.createElement('div')
-div.innerText = 'div'
-document.body.appendChild(div)
+import React from 'react';
+import ReactDom from 'react-dom';
+
+console.log(React)
+console.log(ReactDom)
