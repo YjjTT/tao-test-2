9e5ee0c9d1476b42a5bb72a29068ac07dd3e8834
YjjTT <yjjtt@tempot.com>
2019-09-11 14:27:11 +0800

    import All Icons

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import './importIcons.js'
 
 interface IconProps {
   name: string;
 }
 
-const Icon: React.FunctionComponent<IconProps> = (props) => {
-  return <span>{props.name}</span>;
-}
+const Icon: React.FunctionComponent<IconProps> = props => {
+  return (
+    <span>
+      <svg>
+        <use xlinkHref={`#${props.name}`} />>
+      </svg>
+    </span>
+  );
+};
 export default Icon;
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,4 +2,11 @@ import React from "react";
 import ReactDom from "react-dom";
 import Icon from "./icon";
 
-ReactDom.render(<div><Icon name="wechat"></Icon></div>, document.body);
+ReactDom.render(
+  <div>
+    <Icon name="alipay" />
+    <Icon name="wechat" />
+    <Icon name="qq" />
+  </div>,
+  document.querySelector("#root")
+);
