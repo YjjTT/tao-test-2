b8caa8d07943b67048be5f1ea917daa3c10c39df
YjjTT <yjjtt@tempot.com>
2019-09-16 11:27:30 +0800

    新建icon 目录

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
-import Icon from "./icon";
+import Icon from "./icon/icon";
 
 const fn: React.MouseEventHandler = (e) => {
     console.log(e.target)
