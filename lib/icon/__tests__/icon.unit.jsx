43d7ab4f86b487af4508ed6d098f1bfd680d862f
YjjTT <yjjtt@tempot.com>
2019-09-16 17:40:18 +0800

    icon 测试

--- lib/icon/__tests__/icon.unit.jsx
+++ lib/icon/__tests__/icon.unit.jsx
@@ -1,12 +1,13 @@
 /*
- * @Author: yjjtt 
- * @Date: 2019-09-11 13:19:41 
+ * @Author: yjjtt
+ * @Date: 2019-09-11 13:19:41
  * @Last Modified by: yjjtt
- * @Last Modified time: 2019-09-16 13:10:09
+ * @Last Modified time: 2019-09-16 17:06:27
  */
 import React from "react";
 import renderer from "react-test-renderer";
 import Icon from "../icon";
+import { mount } from "enzyme";
 
 describe("icon", () => {
   it("render success 是个 svg", () => {
@@ -14,7 +15,10 @@ describe("icon", () => {
     expect(json).toMatchSnapshot();
   });
 
-  it("onClick", ()=>{
-      
-  })
+  it("onClick", () => {
+    const fn = jest.fn()
+    const component = mount(<Icon name="alipay" onClick={fn} />);
+    component.find("svg").simulate("click");
+    expect(fn).toBeCalled();
+  });
 });
