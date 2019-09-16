40be1aad48ae1db422a65080a846ca4ac42df2b6
YjjTT <yjjtt@tempot.com>
2019-09-16 11:24:49 +0800

    测试 classes

--- helpers/__tests__/classes.unit.js
+++ helpers/__tests__/classes.unit.js
@@ -1,5 +1,11 @@
-describe('button', ()=>{
-    it('是个div', ()=>{
-
+import classes from '../classes'
+describe('classes', ()=>{
+    it('接受一个className', ()=>{
+        const result = classes('a')
+        expect(result).toEqual('a')
+    })
+    it('接受两个className', ()=>{
+        const result = classes('a', 'b')
+        expect(result).toEqual('a b')
     })
 })
