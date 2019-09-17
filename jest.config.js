61113997aeeb87aa37c2eed7f873889f549e85a5
YjjTT <yjjtt@tempot.com>
2019-09-17 17:56:44 +0800

    JEST_JUNIT_OUTPUT --> JEST_JUNIT_OUTPUT_DIR

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
   coverageReporters: ['text', 'lcov'],
   reporters: ["default", "jest-junit"],
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  moduleDirectories: ['node_modules', 'include'],
+  moduleDirectories: ['node_modules'],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/file-mock.js",
     "\\.(css|less|sass|scss)$": "<rootDir>/test/__mocks__/object-mock.js",
