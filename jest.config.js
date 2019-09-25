5750d4b179c4c7d15196b7161d12c326e4d2b0c4
YjjTT <yjjtt@tempot.com>
2019-09-25 13:51:20 +0800

    add jest.config.ci.js

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,8 @@
 module.exports = {
   verbose: true,
   clearMocks: false,
-  collectCoverage: true,
-  collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov'],
-  reporters: ["default", ["jest-junit", {suiteName: "results"}]],
+  collectCoverage: false,
+  reporters: ["default"],
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   moduleDirectories: ['node_modules'],
   moduleNameMapper: {
