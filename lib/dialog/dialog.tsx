4cee5cd3695064e9aca8ce573e2a1d68e47472a1
YjjTT <yjjtt@tempot.com>
2019-09-26 15:14:08 +0800

    dialog mask 层级问题  ReactDom.CreatePortal

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,4 +1,5 @@
 import React, {Fragment, ReactElement} from 'react'
+import ReactDom from 'react-dom';
 import './dialog.scss'
 import Icon from "../icon/icon";
 import {scopedClassMaker} from "../classes";
@@ -18,31 +19,32 @@ const Dialog: React.FunctionComponent<Props> = (props) => {
     props.onClose(e)
   }
   const onClickMask: React.MouseEventHandler = (e) => {
-    if(props.closeOnClickMask){
+    if (props.closeOnClickMask) {
       props.onClose(e)
     }
   }
-  return (
-    props.visable ?
-      <Fragment>
-        <div className={tui('mask')} onClick={onClickMask}></div>
-        <div className={tui()}>
-          <div className={tui('close')} onClick={onClickClose}>
-            <Icon name="close"/>
-          </div>
-          <header className={tui('header')}>
-            提示
-          </header>
-          <main className={tui('main')}>
-            {props.children}
-          </main>
-          <footer className={tui('footer')}>
-            {props.buttons.map((button, index) => React.cloneElement(button, {key: index}))}
-          </footer>
+  const x = props.visable ?
+    <Fragment>
+      <div className={tui('mask')} onClick={onClickMask}></div>
+      <div className={tui()}>
+        <div className={tui('close')} onClick={onClickClose}>
+          <Icon name="close"/>
         </div>
-      </Fragment>
-      :
-      null
+        <header className={tui('header')}>
+          提示
+        </header>
+        <main className={tui('main')}>
+          {props.children}
+        </main>
+        <footer className={tui('footer')}>
+          {props.buttons.map((button, index) => React.cloneElement(button, {key: index}))}
+        </footer>
+      </div>
+    </Fragment>
+    :
+    null
+  return (
+    ReactDom.createPortal(x, document.body)
   )
 }
 Dialog.defaultProps = {
