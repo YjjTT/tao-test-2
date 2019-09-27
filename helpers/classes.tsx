07a72764016c291765538fb81fc270b261ed2be5
YjjTT <yjjtt@tempot.com>
2019-09-27 15:41:16 +0800

    重构scopedClassMaker

--- helpers/classes.tsx
+++ helpers/classes.tsx
@@ -13,20 +13,21 @@ interface ClassSwitch {
 }
 
 function scopedClassMaker(prefix: string) {
-  return function (name?: string | ClassSwitch, options?: Options) {
-    let name2;
-    let result;
-    if (typeof name === 'string' || name === undefined) {
-      name2 = name;
-      result = [prefix, name2].filter(Boolean).join('-');
-    } else {
-      name2 = Object.entries(name).filter(kv => kv[1]).map(kv => kv[0]);
-      result = name2.map(n => [prefix, n].filter(Boolean).join('-')).join(' ');
-    }
+  return function (name: string | ClassSwitch, options?: Options) {
+    const namesObject = (typeof name === 'string' || name === undefined) ?
+      {[name]: name} :
+      name;
+    const scoped = Object
+      .entries(namesObject)
+      .filter(kv => kv[1] !== false)
+      .map(kv => kv[0])
+      .map(name => [prefix, name].filter(Boolean).join('-'))
+      .join(' ');
+
     if (options && options.extra) {
-      return [result, options && options.extra].filter(Boolean).join(' ');
+      return [scoped, options && options.extra].filter(Boolean).join(' ');
     } else {
-      return result;
+      return scoped;
     }
   };
 }
