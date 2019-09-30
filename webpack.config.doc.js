18f5435ce77bd125c44af5de4451d714317f3a12
YjjTT <yjjtt@tempot.com>
2019-09-30 10:17:42 +0800

    rename example.html

--- webpack.config.doc.js
+++ webpack.config.doc.js
@@ -13,7 +13,7 @@ module.exports = Object.assign({}, base, {
   plugins: [
     new HtmlWebpackPlugin({
       template: "example.html",
-      filename: "example.html"
+      filename: "index.html"
     })
   ]
 });
