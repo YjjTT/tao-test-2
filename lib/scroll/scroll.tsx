280c9af3d06ad3045b224e1ba31f112e49e8446f
YjjTT <yjjtt@tempot.com>
2019-10-24 18:30:29 +0800

    滚动条完成 以及css样式

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
-import {HTMLAttributes} from "react";
+import {HTMLAttributes, useEffect, useRef, useState} from "react";
 import './scroll.scss';
 import scrollbarWidth from './scroll-width';
+import {UIEventHandler} from "react";
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
 
@@ -9,10 +10,31 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
 
 const Scroll: React.FunctionComponent<Props> = (props) => {
   const {children, ...rest} = props;
+  const [barHeight, setBarHeight] = useState(0)
+  const [barTop, setBarTop] = useState(0);
+  const onScroll: UIEventHandler = (e) => {
+    const {current} = containerRef;
+    const scrollHeight = current!.scrollHeight; // 滚动全高
+    const viewHeight = current!.getBoundingClientRect().height; // 可视区域高度
+    const scrollTop = current!.scrollTop;
+    setBarTop(scrollTop * viewHeight / scrollHeight);
+  };
+  const containerRef = useRef<HTMLDivElement>(null);
+  useEffect(()=>{ // mounted的时候 算滚动条的高度
+    const {current} = containerRef;
+    const scrollHeight = current!.scrollHeight; // 滚动全高
+    const viewHeight = current!.getBoundingClientRect().height; // 可视区域高度
+    setBarHeight(viewHeight * viewHeight / scrollHeight);
+  }, []);
   return (
     <div className="tui-scroll" {...rest}>
-      <div className="tui-scroll-inner" style={{right: -scrollbarWidth()}}>
+      <div className="tui-scroll-inner" style={{right: -scrollbarWidth()}}
+           ref={containerRef}
+           onScroll={onScroll}>
         {props.children}
+      </div>
+      <div className="tui-scroll-track">
+        <div className="tui-scroll-bar" style={{height: barHeight, transform: `translateY(${barTop}px)`}} />
       </div>
     </div>
   );
