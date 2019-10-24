f212ae557ba059ae6b1b40cfb62ec25d02ea795c
YjjTT <yjjtt@tempot.com>
2019-10-24 17:10:32 +0800

    JS隐藏滚动条

---
+++ lib/scroll/scroll-width.tsx
@@ -0,0 +1,21 @@
+export default function scrollbarWidth() {
+  if (typeof document === 'undefined') {
+    return 0;
+  }
+
+  const body = document.body;
+  const box = document.createElement('div');
+  const boxStyle = box.style;
+
+  boxStyle.position = 'absolute';
+  boxStyle.top = boxStyle.left = '-9999px';
+  boxStyle.width = boxStyle.height = '100px';
+  boxStyle.overflow = 'scroll';
+
+  body.appendChild(box);
+
+  const width = box.offsetWidth - box.clientWidth;
+
+  body.removeChild(box);
+  return width;
+}
--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import {HTMLAttributes} from "react";
 import './scroll.scss';
+import scrollbarWidth from './scroll-width';
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
 
@@ -10,7 +11,7 @@ const Scroll: React.FunctionComponent<Props> = (props) => {
   const {children, ...rest} = props;
   return (
     <div className="tui-scroll" {...rest}>
-      <div className="tui-scroll-inner">
+      <div className="tui-scroll-inner" style={{right: -scrollbarWidth()}}>
         {props.children}
       </div>
     </div>
